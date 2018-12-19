0225717045487ded476acedfc5307a5e72ade8a7
Michael Rodgers <mail@mjrodge.me>
2018-12-19 15:47:24 -0500

    Added Firebase Config

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { View, Text } from 'react-native';
+import reducers from './reducers';
+import firebase from 'firebase';
+
+import firebaseConfig from '../firebaseConfig';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello</Text>
         </View>
