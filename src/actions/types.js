b65a58126da17c0a6c7908d98501fdddd5fe9c27
Michael Rodgers <mail@mjrodge.me>
2019-01-03 13:14:10 -0500

    Scaffold for Employee Form Reducer

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const password_changed = 'password_changed';
 export const login_user_success = 'login_user_success';
 export const login_user_fail = 'login_user_fail';
 export const login_user = 'login_user';
+
+export const EMPLOYEE_UPDATE = 'employee_update';
