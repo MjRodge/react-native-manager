74041daa7ee9c9974f0f213d72a25f9b0a0567d3
Michael Rodgers <mail@mjrodge.me>
2019-01-03 12:41:16 -0500

    Scaffold for Employee Creation Form Scene

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/loginForm';
 import EmployeeList from './components/employeeList';
+import EmployeeCreate from './components/employeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -15,6 +16,16 @@ const RouterComponent = () => {
             key="employeeList"
             component={EmployeeList}
             title="Employee Listings"
+            rightTitle="Add"
+            onRight={() => {
+              Actions.employeeCreate();
+            }}
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            title="Create Employee"
+            component={EmployeeCreate}
           />
         </Scene>
       </Scene>
