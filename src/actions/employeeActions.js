e63fa649b5597f8dcdd1228703a33630e57beac4
Michael Rodgers <mail@mjrodge.me>
2019-01-24 16:36:56 -0500

    Used Redux-Thunk to send Firebase storage request

--- src/actions/employeeActions.js
+++ src/actions/employeeActions.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { employee_update } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -8,5 +10,13 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift);
+  const { currentUser } = firebase.auth();
+
+  return () => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => Actions.employeeList({ type: 'reset' }));
+  };
 };
